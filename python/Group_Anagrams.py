bc353ae84665a2546774eace4d0e9beac2102256
justinYoo <ugh1118@gmail.com>
2023-10-10 08:49:43 +0900

    improve previous code

--- python/Group_Anagrams.py
+++ python/Group_Anagrams.py
@@ -1,7 +1,17 @@
 # https://leetcode.com/problems/group-anagrams/description/
 
+from collections import defaultdict
+
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        anagrams = defaultdict(list)
+
+        for word in strs:
+            # 정렬한 값을 키로하여 딕셔너리에 추가
+            anagrams[''.join(sorted(word))].append(word)
+        return list(anagrams.values())
+        
+        '''
         sorted_strs = [''.join(sorted(list(word))) for word in strs]
 
         sorted_dict = dict()
@@ -15,3 +25,4 @@ class Solution:
         for index_list in sorted_dict.values():
             ret.append([strs[index] for index in index_list])
         return ret
+        '''
