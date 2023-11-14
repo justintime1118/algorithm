be0fa6be1e297d81301845f93c16298952108aab
justinYoo <ugh1118@gmail.com>
2023-11-14 21:59:40 +0900

    refactor by adding direction array

--- java/programmers/triangleSnail.java
+++ java/programmers/triangleSnail.java
@@ -3,44 +3,35 @@ package programmers;
 import java.util.*;
 
 class Solution {
+
+    private static final int[] dx = {0, 1, -1};
+    private static final int[] dy = {1, 0, -1};
+
     public int[] solution(int n) {
         
         // 삼각형 그릴 이차원 배열
         int[][] grid = new int[n][n];
         
-        // initGrid(grid);
-        
         int i = 0;
         int j = 0;
         int num = 1;
+        int dir = 0;
         grid[i][j] = num;
-        
         while (num < n*(n+1)/2) {
-            // 아래로 이동
-            while (i + 1 < n && grid[i + 1][j] == 0) {
-                i++;
+            
+            while (i + dy[dir] < n && j + dx[dir] < n && grid[i + dy[dir]][j + dx[dir]] == 0) {
+                i += dy[dir];
+                j += dx[dir];
                 num++;
-                grid[i][j] = num;
+                grid[i][j] = num;                
             }
-            // 오른쪽으로 이동하며 마킹
-            while (j + 1 < n && grid[i][j + 1] == 0) {
-                j++;
-                num++;
-                grid[i][j] = num;
-            }
-            // 왼쪽 위로 이동하며 마킹
-            while (0 < i - 1 && 0 < j - 1 && grid[i - 1][j - 1] == 0) {
-                i--;
-                j--;
-                num++;
-                grid[i][j] = num;
-            }
+            dir++;
+            dir %= 3;
         }
         
         // printGrid(grid);
 
         List<Integer> ans = new ArrayList<>();
-        
         
         for (int r = 0; r < n; r++) {
             for (int c = 0; c < n; c++) {
