e4015441430daa3b48702558e71c787d8176a5af
justinYoo <ugh1118@gmail.com>
2023-11-19 21:43:23 +0900

    add other solutions

--- java/programmers/numberToReversedArray.java
+++ java/programmers/numberToReversedArray.java
@@ -13,4 +13,32 @@ class Solution {
         }
         return arr.stream().mapToInt(x -> (int)x).toArray();
     }
+
+    /* 내가 생각해낸 다른풀이 
+    public int[] solution(long n) {
+        char[] charArr = (n + "").toCharArray();
+        int[] ans = new int[charArr.length];
+        
+        for (int i = charArr.length - 1; i >= 0; i--) {
+            // ans[charArr.length - 1 - i] = Integer.valueOf(charArr[i]) - Integer.valueOf('0'); // 아래와 같은 결과
+            ans[charArr.length - 1 - i] = Character.getNumericValue(charArr[i]);
+        }
+        return ans;
+    }
+    */
+
+    /* 책에서 소개된 풀이 (문자열 클래스 이용)
+    public int[] solution(long n) {
+        String str = Long.toString(n);
+        String reversed = new StringBuilder(str).reverse().toString();
+        char[] arr = reversed.toCharArray();
+        
+        int[] result = new int[arr.length];
+        for (int i = 0; i < result.length; i++) {
+            result[i] = arr[i] - '0';
+        }
+        
+        return result;
+    }
+     */
 }
