ce12dddb8dd665c757dc6a191151649b9a940ba2
justinYoo <ugh1118@gmail.com>
2023-12-27 15:34:03 +0900

    add textbook example code

--- java/programmers/VowelDictionary.java
+++ java/programmers/VowelDictionary.java
@@ -2,8 +2,31 @@ package programmers;
 
 // 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/84512
 
-// 정답 맞힌 코드
+// 교재에 수록된 보다 최적화된 코드
 
+import java.util.*;
+
+class Solution {
+    private static final char[] CHARS = "AEIOU".toCharArray();
+
+    private void generate(String word, List<String> words) {
+        words.add(word);
+
+        if (word.length() == 5) return;
+        for (char c : CHARS) {
+            generate(word + c, words);
+        }
+    }
+
+    public int solution(String word) {
+        List<String> words = new ArrayList<>();
+        generate("", words);
+        return words.indexOf(word);
+    }
+}
+
+// 내가 푼 정답 코드
+/*
 class Solution {
 
     int order;
@@ -33,61 +56,6 @@ class Solution {
             vowelDict(depth + 1, new String(currentString + "I"), word);
             vowelDict(depth + 1, new String(currentString + "O"), word);
             vowelDict(depth + 1, new String(currentString + "U"), word);
-        }
-    }
-}
-
-// 이하 삽질한 코드
-/*
-import java.util.*;
-
-class Solution {
-    
-    int order = 1;
-    String finalWord;
-    
-    Map<Integer, String> alphaMap = new HashMap<>();
-    
-    public int solution(String word) {
-        
-        alphaMap.put(0, "A");
-        alphaMap.put(1, "E");
-        alphaMap.put(2, "I");
-        alphaMap.put(3, "O");
-        alphaMap.put(4, "U");
-        
-        vowelDict("A", 'A', word, 1);
-        System.out.println("finalWord == " + finalWord);
-        return order;
-    }
-    
-    // 메서드 파라미터로 상태 정의
-    // 아래 재귀 규칙과 아귀가 맞아야 함
-    public void vowelDict(String currentWord, int currentAlpha, String word, int depth) {
-        if (order > 30)
-            return;
-        // base condition
-        if (word.equals(currentWord)) {
-            finalWord = currentWord;
-            return;
-        }
-        if (depth > 5) {
-            return;
-        }
-        
-        System.out.println(currentWord);
-        
-        // recursive logic
-        if (!word.equals(currentWord)) {
-            // 1. 자릿수 하나 키우고 새로 생겨난 자리에 A 채우기
-            vowelDict(new String(currentWord + "A"), 0, word, depth + 1);
-            order++;
-            
-            // 2. 현재 위치에서 다음 알파벳으로 올리기
-            String nextWord = currentWord.substring(currentWord.length() - 1);
-            nextWord += alphaMap.get((currentAlpha + 1) % 5);
-            vowelDict(nextWord, (currentAlpha + 1) % 5, word, depth);
-            order++;
         }
     }
 }
