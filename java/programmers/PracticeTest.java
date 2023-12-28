ad4bd6e8e93c31b9bf0e6974465a352d7953f59e
justinYoo <ugh1118@gmail.com>
2023-12-28 17:44:43 +0900

    refactor: introduce IntStream

--- java/programmers/PracticeTest.java
+++ java/programmers/PracticeTest.java
@@ -2,37 +2,31 @@ package programmers;
 
 // 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/42840
 
-import java.util.*;
+import java.util.stream.IntStream;
 
 class Solution {
+
+    private static final int[][] RULES = {
+        {1, 2, 3, 4, 5},
+        {2, 1, 2, 3, 2, 4, 2, 5},
+        {3, 3, 1, 1, 2, 2, 4, 4, 5, 5}
+    };
+
     public int[] solution(int[] answers) {
+        int[] scores = new int[3];
         int maxScore = 0;
-        
-        int[] submission1 = {1, 2, 3, 4, 5};
-        int score1 = checkScore(answers, submission1);
-        maxScore = Math.max(maxScore, score1);
-        
-        int[] submission2 =  {2, 1, 2, 3, 2, 4, 2, 5};
-        int score2 = checkScore(answers, submission2);
-        maxScore = Math.max(maxScore, score2);
-        
-        int[] submission3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
-        int score3 = checkScore(answers, submission3);
-        maxScore = Math.max(maxScore, score3);
-        
-        ArrayList<Integer> res = new ArrayList<>();
-        if (score1 == maxScore)
-            res.add(1);
-        if (score2 == maxScore)
-            res.add(2);
-        if (score3 == maxScore)
-            res.add(3);
-        
-        int[] result = new int[res.size()];
-        for (int i = 0; i < res.size(); i++) {
-            result[i] = res.get(i);
+
+        for (int i = 0; i < 3; i++) {
+            scores[i] = checkScore(answers, RULES[i]);
+            maxScore = Math.max(maxScore, scores[i]);
         }
-        return result;
+        
+        final int MAX_SCORE = maxScore;
+
+        return IntStream.range(0, 3)
+                .filter(i -> scores[i] == MAX_SCORE)
+                .map(i -> i + 1)
+                .toArray();
     }
     
     public int checkScore(int[] answers, int[] submission) {
@@ -44,21 +38,3 @@ class Solution {
         return score;
     }
 }
-/*
-1번 -> 1~5 반복
-- submit1 = [1, 2, 3, 4, 5]
-- if (submit1[idx % 5] == answers[idx]) -> 정답!
-
-
-2번 -> 21232425 반복
-- submit2 = [2, 1, 2, 3, 2, 4, 2, 5]
-- if (submit1[idx % 8] == answers[idx]) -> 정답!
-
-3번 -> 3311224455 반복
-- submit3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
-- if (submit1[idx % 10] == answers[idx]) -> 정답!
-
-위 방법을 이용하여 각 수포자별 점수 채점!
-
-최고점 동점자 존재할 경우, 번호를 기준으로 오름차순으로 정렬한다
-*/
