f4160d2dbe6500a9b73ebe67d7516708ca88a4a4
justinYoo <ugh1118@gmail.com>
2024-01-03 14:45:05 +0900

    solve with same logic, but different code

--- java/programmers/Carpet.java
+++ java/programmers/Carpet.java
@@ -2,38 +2,49 @@ package programmers;
 
 // 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/42842
 
-// 전에 풀어 둔 코드
 class Solution {
     public int[] solution(int brown, int yellow) {
         int[] ans = new int[2];
-        int total = brown + yellow;
-        int width = 0;
-        int height = 0;
-        // brown + yellow를 나머지 없이 딱 나눌 수 있는 수를 3부터 시작해서 구한다.
-        // 그러면 세로 * 가로 길이가 될 수 있는 쌍이 나온다.
-        // 걔네가 주어진 brown과 yellow가 나오는 애들인지 확인하면 됨
         
-        for (int i = 3; total / i >= i; i++) {
-            if (total % i != 0)
-                continue ;
-            height = i;
-            width = total / i;
-            if (check(width, height, yellow) == true) {
-                ans[0] = width;
-                ans[1] = height;
-                break ;
+        int totalGrid = brown + yellow;
+        int outerGrid = 0;
+        int width = 0;
+        int height = 3;
+        
+        while (height <= (int)Math.sqrt(totalGrid)) {
+            
+            width = (totalGrid % height == 0) ? (totalGrid / height) : 0;
+            if (width == 0) {
+                height++;
+                continue;
             }
+            
+            // 현재 가로 세로 길이로 계산한 바깥쪽 격자 갯수와 brown이 같으면 정답
+            outerGrid = width*2 + (height - 2)*2;
+            if (outerGrid == brown)
+                break;
+            
+            height++;
         }
+        
+        ans[0] = width;
+        ans[1] = height;
         return ans;
     }
-    
-    public boolean check(int width, int height, int yellow) {
-        int checkYellow = (width - 2) * (height - 2);
-
-        // 최종확인
-        if (checkYellow == yellow)
-            return (true);
-        else
-            return (false);
-    }
 }
+
+/*
+<전체 풀이 로직>
+반복문으로 height를 3에서부터 1씩 키워가면서
+    1) 가로길이 도출. 단, (totalGrid) % height == 0
+    2) outerGrid 갯수가 brown과 같아질 때까지 반복
+
+결과적으로 width와 height 도출 가능
+
+<계산 로직>
+*totalGrid: brown + yellow
+*width: (totalGrid % height == 0) ? (totalGrid / height) : 0
+*outerGrid: width*2 + (height-2)*2
+*innerGrid: width*height - outerGrid
+-> 전체 격자 갯수에 맞춰서 가로 세로 길이를 구했으므로 outerGrid가 맞으면 논리적으로 innerGrid는 당연히 맞으므로 따로 체크를 안해도 됨
+*/
