c5218c57fe637ae02c6936bb77e08534a6ba9277
justinYoo <ugh1118@gmail.com>
2024-01-16 17:49:24 +0900

    improved solution using stream (by textbook)

--- java/programmers/SumSelectedTwo.java
+++ java/programmers/SumSelectedTwo.java
@@ -7,18 +7,13 @@ import java.util.*;
 class Solution {
     public int[] solution(int[] nbr) {
         Set<Integer> nbrSet = new HashSet<>();
+
         for (int i = 0; i < nbr.length - 1; i++) {
             for (int j = i + 1; j < nbr.length; j++) {
                 nbrSet.add(nbr[i] + nbr[j]);
             }
         }
-        Integer[] nbrSetArr = nbrSet.toArray(new Integer[0]);
-        Arrays.sort(nbrSetArr);
-        
-        int[] ans = new int[nbrSetArr.length];
-        for (int i = 0; i < nbrSetArr.length; i++) {
-            ans[i] = (int)nbrSetArr[i];
-        }
-        return ans;
+
+        return nbrSet.stream().mapToInt(Integer::intValue).sorted().toArray();
     }
 }
