2fcb922b27e7d212906c786f89e5fa94f5b89712
justinYoo <ugh1118@gmail.com>
2024-02-17 16:52:22 +0900

    refactor(refer from textbook)

--- java/programmers/BiggestNumber.java
+++ java/programmers/BiggestNumber.java
@@ -1,26 +1,18 @@
 package programmers;
 
 // 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/42746
-import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 class Solution {
     public String solution(int[] numbers) {
-        ArrayList<String> arrlst = new ArrayList<>();
-        for (int number : numbers) {
-            arrlst.add(String.valueOf(number));
-        }
-        String[] strArr = arrlst.stream().sorted((s1, s2) -> {
-            return (s2 + s1).compareTo(s1 + s2);
-        }).toArray(String[]::new);
-
-        String ans = String.join("", strArr);
-
-        char[] charArr = ans.toCharArray();
-        for (char c : charArr) {
-            if (c != '0')
-                return ans;
-        }
-
-        return "0";
+        String ans = Arrays.stream(numbers)
+                .mapToObj(String::valueOf)
+                .sorted((s1, s2) -> {
+                    return (s2 + s1).compareTo(s1 + s2);
+                })
+                .collect(Collectors.joining(""))
+                .replaceAll("^0+", "0");
+        return ans;
     }
 }
