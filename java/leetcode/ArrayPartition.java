c0b072a8abaa4ecb6418099e26e00e4fb1385473
justinYoo <ugh1118@gmail.com>
2024-03-25 21:05:16 +0900

    [solve] 561. Array Partition

---
+++ java/leetcode/ArrayPartition.java
@@ -0,0 +1,22 @@
+package leetcode;
+
+//https://leetcode.com/problems/array-partition/description/
+
+import java.util.Arrays;
+
+class Solution {
+    public int arrayPairSum(int[] nums) {
+        // 배열 정렬
+        Arrays.sort(nums);
+
+        // 짝수 인덱스만 골라서 더한 후 리턴
+        // (0,1), (2,3), ...으로 짝을 이룬 뒤 더 작은 것을 고르는 것과 같음
+        int ans = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (i % 2 != 0)
+                continue;
+            ans += nums[i];
+        }
+        return ans;
+    }
+}
