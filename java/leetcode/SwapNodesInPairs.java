68fd5460764e2d256c3583ebee15ccba8a1ad6b6
justinYoo <ugh1118@gmail.com>
2024-04-17 15:51:52 +0900

    [refactor] recursive solution

--- java/leetcode/SwapNodesInPairs.java
+++ java/leetcode/SwapNodesInPairs.java
@@ -1,7 +1,5 @@
 package leetcode;
 
-import java.util.LinkedList;
-
 //문제링크: https://leetcode.com/problems/swap-nodes-in-pairs/
 
 /**
@@ -15,39 +13,16 @@ import java.util.LinkedList;
  * }
  */
 class Solution {
-    public ListNode swapPairs(ListNode head) {
-        if (head == null)
-            return null;
-        if (head.next == null)
-            return head;
-
-        int size = 0;
-        ListNode cur = head;
-        LinkedList<ListNode> stack = new LinkedList<>();
-        while (cur != null) {
-            stack.push(cur);
-            cur = cur.next;
+    public ListNode swapPairs(ListNode node) {
+        // base condition
+        if (node == null || node.next == null) {
+            return node;
         }
 
-        ListNode prev = null;
-        // List의 길이가 홀수인 경우 처리
-        if (stack.size() % 2 != 0) {
-            prev = stack.pop();
-        }
-
-        ListNode adj1 = null;
-        ListNode adj2 = null;
-        ListNode tmp = null;
-        // 1->2로 되어 있던 것을 2->1로 swap
-        while (stack.size() > 0) {
-            adj2 = stack.pop();
-            adj1 = stack.pop();
-
-            adj1.next = prev;
-            adj2.next = adj1;
-            prev = adj2;
-        }
-
-        return prev;
+        // recursive logic
+        ListNode first = node.next;
+        node.next = swapPairs(node.next.next);
+        first.next = node;
+        return first;
     }
 }
