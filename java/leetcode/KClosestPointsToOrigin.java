33f71120580c0c8f1640af78b73e1cd25b63663f
justinYoo <ugh1118@gmail.com>
2024-04-30 17:08:41 +0900

    [2nd solution] apply priority queue

--- java/leetcode/KClosestPointsToOrigin.java
+++ java/leetcode/KClosestPointsToOrigin.java
@@ -1,15 +1,31 @@
 package leetcode;
 
-import java.util.Arrays;
+import java.util.PriorityQueue;
 
 //문제링크: https://leetcode.com/problems/k-closest-points-to-origin/
 
 class Solution {
-    public int[][] kClosest(int[][] points, int k) {
-        Arrays.sort(points, (p1, p2) -> {
-            return (p1[0] * p1[0] + p1[1] * p1[1]) - (p2[0] * p2[0] + p2[1] * p2[1]);
-        });
+    private class Point {
+        public int[] point;
+        public int distance;
 
-        return Arrays.copyOfRange(points, 0, k);
+        public Point(int[] point) {
+            this.point = point;
+            // 대소비교만 하면 되므로 제곱근 처리는 생략한다
+            this.distance = point[0] * point[0] + point[1] * point[1];
+        }
+    }
+
+    public int[][] kClosest(int[][] points, int k) {
+        PriorityQueue<Point> pq = new PriorityQueue<>((p1, p2) -> p1.distance - p2.distance);
+        for (int[] p : points)
+            pq.add(new Point(p));
+
+        int[][] ans = new int[k][2];
+
+        for (int i = 0; i < k; i++)
+            ans[i] = pq.poll().point;
+
+        return ans;
     }
 }
