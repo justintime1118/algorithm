f9e83aade2364fc4bbedbcd519b6cc38ec9278da
justinYoo <ugh1118@gmail.com>
2024-05-01 12:47:24 +0900

    [refactor] reduce space complexity

--- java/leetcode/NumberOfIslands.java
+++ java/leetcode/NumberOfIslands.java
@@ -6,15 +6,13 @@ class Solution {
     // 동, 서, 남, 북
     int[] dx = { 1, -1, 0, 0 };
     int[] dy = { 0, 0, 1, -1 };
-    boolean[][] visited;
 
     public int numIslands(char[][] grid) {
-        visited = new boolean[grid.length][grid[0].length];
 
         int ans = 0;
         for (int col = 0; col < grid.length; col++) {
             for (int row = 0; row < grid[0].length; row++) {
-                if (!visited[col][row] && grid[col][row] == '1') {
+                if (grid[col][row] == '1') {
                     ans++;
                     markIsland(col, row, grid);
                 }
@@ -27,11 +25,10 @@ class Solution {
     public void markIsland(int col, int row, char[][] grid) {
         if (!(0 <= col && col < grid.length)
                 || !(0 <= row && row < grid[0].length)
-                || grid[col][row] == '0'
-                || visited[col][row])
+                || grid[col][row] == '0')
             return;
 
-        visited[col][row] = true;
+        grid[col][row] = '0';
         for (int dir = 0; dir < 4; dir++) {
             markIsland(col + dy[dir], row + dx[dir], grid);
         }
