8ae97763103edd7f4c6694ad0a16fe97ce07113a
justinYoo <ugh1118@gmail.com>
2024-06-19 13:35:52 +0900

    [refactor] clean logic

--- java/leetcode/LongestUnivaluePath.java
+++ java/leetcode/LongestUnivaluePath.java
@@ -1,8 +1,4 @@
-package leetcode;
-
 import javax.swing.tree.TreeNode;
-
-//https://leetcode.com/problems/longest-univalue-path/
 
 /**
  * Definition for a binary tree node.
@@ -36,27 +32,17 @@ class Solution {
         int left = dfs(node.left);
         int right = dfs(node.right);
 
-        int maxLen = 0;
-        // 양쪽 자식노드 모두 univalue이면 maxLen 갱신
-        if (node.left != null && node.val == node.left.val
-                && node.right != null && node.val == node.right.val) {
-            maxLen += left + right + 2;
-        }
-
-        int leftDepth = 0;
-        int rightDepth = 0;
-        // 양쪽 자식노드에 대해 각각 univalue이면 depth 갱신
         if (node.left != null && node.val == node.left.val)
-            leftDepth = left + 1;
+            left += 1;
+        else
+            left = 0;
         if (node.right != null && node.val == node.right.val)
-            rightDepth = right + 1;
+            right += 1;
+        else
+            right = 0;
 
-        int maxDepth = Math.max(leftDepth, rightDepth);
+        ans = Math.max(ans, left + right);
 
-        // 현재 노드가 만들어 낼 수 있는 가장 긴 경로를 ans에 비교 반영
-        ans = Math.max(ans, Math.max(maxLen, maxDepth));
-
-        return maxDepth;
-
+        return Math.max(left, right);
     }
 }
