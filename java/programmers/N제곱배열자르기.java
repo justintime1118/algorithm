9a967d4bc574a4f591dd3bdb1c7ee8e8274701a2
justinYoo <ugh1118@gmail.com>
2024-08-19 11:16:31 +0900

    [refactor] apply stream API

--- java/programmers/N제곱배열자르기.java
+++ java/programmers/N제곱배열자르기.java
@@ -1,22 +1,13 @@
 package programmers;
 
+// https://school.programmers.co.kr/learn/courses/30/lessons/87390
+
+import java.util.stream.LongStream;
+
 class Solution {
     public int[] solution(int n, long left, long right) {
-        int len = (int) right - (int) left + 1;
-        int[] ans = new int[len];
-
-        for (int i = 0; i < ans.length; i++) {
-            long idx = left + i;
-            // 값이 채워지는 것은 0부터가 아닌 1부터이므로 1을 더해줌
-            long row = idx / n + 1;
-            long col = idx % n + 1;
-
-            ans[i] = col <= row ? (int) row : (int) col;
-        }
-
-        return ans;
+        return LongStream.rangeClosed(left, right)
+                .mapToInt(idx -> (int) Math.max(idx / n, idx % n) + 1)
+                .toArray();
     }
 }
-/*
- * 명확한 규칙성을 가지고 숫자가 채워지므로, 특정 인덱스에 대한 값을 계산해낼 수 있다
- */
