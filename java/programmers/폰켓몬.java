435bb18a2833803cf7dfcc13551baf237cb0c3da
justinYoo <ugh1118@gmail.com>
2024-10-09 11:37:03 +0900

    [refactor] stream과 Math.min()을 사용한 훨씬 단순한 코드

--- java/programmers/폰켓몬.java
+++ java/programmers/폰켓몬.java
@@ -1,20 +1,9 @@
 package programmers;
 
-//https://school.programmers.co.kr/learn/courses/30/lessons/1845
-
-import java.util.HashSet;
-import java.util.Set;
+import java.util.Arrays;
 
 class Solution {
     public int solution(int[] nums) {
-        Set<Integer> set = new HashSet<>();
-        for (int num : nums)
-            set.add(num);
-
-        int n = nums.length;
-
-        // 전체 종류의 개수가 n/2보다 크면 모두 다른 다른 종류로 고를 수 있음
-        // 전체 종류의 개수가 n/2보다 작으면 모든 종류를 사용함
-        return set.size() >= n / 2 ? n / 2 : set.size();
+        return Math.min((int) Arrays.stream(nums).distinct().count(), nums.length / 2);
     }
 }
